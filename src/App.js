966d9f9e259b1bf9601488c1be80f406667389c5
Marcin Hagmajer <mhagmajer@gmail.com>
2019-11-08 16:32:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { LineChart, Line } from 'recharts';
+import {
+  Grid,
+  Segment,
+} from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
+
+const data = [
+  { name: 'Page A', uv: 100 },
+  { name: 'Page B', uv: 300 },
+  { name: 'Page C', uv: 200 },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid celled container columns={2} stackable>
+      <Grid.Row columns={1}>
+        <Grid.Column>
+          <Segment>Content</Segment>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row columns={2}>
+        <Grid.Column width={4}>
+          <Segment>Content</Segment>
+        </Grid.Column>
+        <Grid.Column width={12}>
+          <Segment>
+            <LineChart width={400} height={400} data={data}>
+              <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            </LineChart>
+          </Segment>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   );
 }
 
